ab5158c6030ae2de5fbed6ca5408f6cbf7ff19af
Don <71207900+VaperinaDEV@users.noreply.github.com>
2023-10-12 10:06:31 +0200

    Add files via upload

---
+++ javascripts/discourse/initializers/solved-badge.js
@@ -0,0 +1,76 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import discourseComputed from "discourse-common/utils/decorators";
+import TopicListItem from "discourse/components/topic-list-item";
+
+export default {
+  name: "solved-badge",
+
+  initialize(container) {
+    withPluginApi("0.8", api => {
+      
+      TopicListItem.reopen({
+        @discourseComputed()
+        unboundClassNames() {
+          let classList = this._super(...arguments);
+          if (this.topic.can_have_answer) {
+            classList += " solvable";
+          }
+          return classList;
+        },
+      });
+
+      api.onAppEvent("page:topic-loaded", (topic) => {
+        if (!topic) {
+          return;
+        }
+        if (topic.can_have_answer) {
+          document.body.classList.add("solvable-topic");
+        } else {
+          document.body.classList.remove("solvable-topic");
+        }        
+        
+        if (topic.accepted_answer) {
+          document.body.classList.add("solved-topic");
+        } else {
+          document.body.classList.remove("solved-topic");
+        }        
+      });
+      
+      api.decorateTopicTitle((topic) => {
+        const solvedTopic = document.body.classList.contains("solved-topic");
+        const solvableTopic = document.body.classList.contains("solvable-topic");
+        
+        if (topic.accepted_answer) {
+          if (!solvedTopic) {
+            document.body.classList.add("solved-topic");
+          }
+        }
+
+        const acceptedButton = document.querySelector("button.accepted");
+        this.siteSettings = container.lookup("service:site-settings");
+        
+        acceptedButton?.addEventListener("click", () => {
+          if (!solvableTopic && this.siteSettings.empty_box_on_unsolved) {
+            document.body.classList.add("solvable-topic");
+          }
+          if (solvedTopic) {
+            document.body.classList.remove("solved-topic");
+          }
+        });
+      });
+
+      api.onPageChange((url, title) => {
+        const solvableTopic = document.body.classList.contains("solvable-topic");
+        const solvedTopic = document.body.classList.contains("solved-topic");
+        const topicPage = document.body.classList.contains("archetype-regular");
+
+        if (!topicPage && solvableTopic) {
+          document.body.classList.remove("solvable-topic");
+        }
+        if (!topicPage && solvedTopic) {
+          document.body.classList.remove("solved-topic");
+        }        
+      });      
+    });
+  }
+};
